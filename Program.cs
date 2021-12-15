59e60bbf72807cf598662a5486670885da829ba2
Joy Evans <joyevans@me.com>
2021-12-15 09:06:19 -0600

    modifying main menu

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, Welcome to CrownReady Beauty Supply!");
+Console.WriteLine("The place where you find ...");
+
+Console.WriteLine("Login or Create an account")
+// If user input == Login, ask for user.email
+// If user input == Create an account, ask for user.name and user.email
+// else, ask user to "Try again" and loop back to main menu
+
+Console.WriteLine("Let's get started! What would you like to do today?");
+
