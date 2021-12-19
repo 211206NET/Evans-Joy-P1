635c3fb6e57d29899ef3fe18d6b34430aec852e5
Joy Evans <joyevans@me.com>
2021-12-19 14:11:51 -0600

    added xml documentation

--- CrownReady/DL/StaticStorage.cs
+++ CrownReady/DL/StaticStorage.cs
@@ -5,10 +5,28 @@ namespace DL;
 // A static class in C# is a class that cannot be instantiated.
 // A static class can only contain static data members including static methods, static constructors, and static properties.
 // In C#, a static class is a class that cannot be instantiated. ... You can't create an object for the static class.
-public static class StaticStorage
+public class StaticStorage
 {
 
-    public static List<StoreFrontDetail> allLocations = new List<StoreFrontDetail>();
+    private static List<StoreFront> _allStoreFronts = new List<StoreFront>();
+
+    /// <summary>
+    /// Returns all storefronts from _allStoreFronts list
+    /// </summary>
+    /// <returns>all storefronts in the list</returns>
+    public List<StoreFront> GetAllStores()
+    {
+        return StaticStorage._allStoreFronts;
+    }
+
+    /// <summary>
+    /// add a new storefront to the list
+    /// </summary>
+    /// <param name="storeFront">a new storefront object to add</param>
+    public void AddStoreFront (StoreFront storeFront)
+    {
+        StaticStorage._allStoreFronts.Add(storeFront);
+    }
 
 
 }
