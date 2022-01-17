4fb3efbfed8e322441c57e375c83bd1aa48ad49f
“Joy” <joyevans@me.com>
2022-01-17 14:25:29 -0600

    Created new test for controller

---
+++ CrownReady/Tests/ControllerTest.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Tests;
+
+public class ControllerTest
+{
+    [Fact]
+    public void ControllerTestShouldWork()
+    {
+        bool b = true;
+        Assert.True(b);
+    }
+}
